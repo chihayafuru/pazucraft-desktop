e21c165abf5afc42a12d027b626ef223afb3b255
Takehiko YOSHIDA <chihayafuru@gmail.com>
2016-12-06 23:53:06 +0900

    update the menu

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,54 @@
+import {remote} from "electron";
+const app = remote.app;
+const Menu = remote.Menu;
+const MenuItem = remote.MenuItem;
+
 class StartUp {
+
+    private browserWindow = remote.getCurrentWindow();
+
+    private template = [
+        {
+            label: 'File',
+            submenu:[
+                {
+                    label: 'Open',
+                    click: (item, focusedWindow) => {
+                        console.log("open()");
+                    }
+                },
+                {
+                    label: 'Convert',
+                    click: (item, focusedWindow) => {
+                        console.log("convert()");
+                    }
+                },
+                {
+                    label: 'Save',
+                    click: (item, focusedWindow) => {
+                        console.log("save()");
+                    }
+                },
+                {
+                    label: 'Toggle Developer Tools',
+                    click: (item, focusedWindow) => {
+                        this.browserWindow.webContents.toggleDevTools();
+                    }
+                },
+                {
+                    label: 'Quit',
+                    click: (item, focusedWindow) => {
+                        app.quit();
+                    }
+                }
+            ]
+        }
+    ];
+    
     constructor() {
+        let menu = Menu.buildFromTemplate(this.template);
+        Menu.setApplicationMenu(menu);
+
         let para = document.getElementById("greeting");
         para.innerText = "Hello Electron App!";
     }
