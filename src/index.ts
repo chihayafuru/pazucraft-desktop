eb9200d34532d2ce65f49ff4d1d4bca3a8bb7a55
Takehiko YOSHIDA <chihayafuru@gmail.com>
2016-12-06 21:25:14 +0900

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+class StartUp {
+    constructor() {
+        let para = document.getElementById("greeting");
+        para.innerText = "Hello Electron App!";
+    }
+}
+
+const startUp = new StartUp()
